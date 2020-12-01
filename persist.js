4bc4eef6d75ce7148034415f82db9bb0f9054281
josh <joshua@marinacci.org>
2020-12-01 11:48:41 -0800

    append json to all files

--- persist.js
+++ persist.js
@@ -6,9 +6,8 @@ const JSON_DIR = "json"
 export async function persist_save(id, body) {
     console.log("saving to id", id, 'with body', body)
     try {
-        let blob_path = path.join(JSON_DIR, id)
+        let blob_path = path.join(JSON_DIR, id+'.json')
         let access = await fs.access(JSON_DIR)
-        access = await fs.access(blob_path)
         let done = await fs.writeFile(blob_path, JSON.stringify(body))
         console.log("done writing")
         return {
@@ -27,7 +26,7 @@ export async function persist_save(id, body) {
 export async function persist_load(id) {
     console.log("loading from id", id)
     try {
-        let blob_path = path.join(JSON_DIR, id)
+        let blob_path = path.join(JSON_DIR, id+'.json')
         let access = await fs.access(JSON_DIR)
         access = await fs.access(blob_path)
         let data = await fs.readFile(blob_path)
